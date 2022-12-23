96893112ac7d3f772a926991cb53fa135b2b030d
Daniel <danielspradah0@gmail.com>
2022-12-22 22:26:56 -0500

    Add board and player module

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,9 +1,41 @@
 /* eslint-disable semi */
+/* eslint-disable no-unused-vars */
+
+// CONSTRUCTORS
+
+const board = (() => {
+
+    const board = []
+
+    const markCell = (shape, cell) => {
+        cellList[cell].innerHTML = shape;
+    }
+    
+    return {markCell};
+
+})();
+
+
+const playerFactory = (name, shape) => {
+    return {name, shape};  
+}
+
 
 // LEFT SIDE 
 const body = document.getElementById('body');
 const popup = document.querySelector('.naming-container');
 const form = document.querySelector('.player-name-form');
+const inputs = document.querySelectorAll('.input');
+
+
+//BOARD
+
+const cellList = document.querySelectorAll('.board-btn')
+
+for (let i = 0; i < cellList.length; i++) {
+    cellList[i].id = `${i}`;
+}
+
 const player1Name = document.getElementById('player1-name');
 const player2Name = document.getElementById('player2-name');
 const playerNameBtn = document.getElementById('player-name-btn');
@@ -34,3 +66,16 @@ body.addEventListener('click', () => {
     //   body.style.backgroundColor = 'rgba(0, 0, 0, 0)';
     // }
   })
+
+for (let i = 0; i < inputs.length; i++) {
+    inputs[i].addEventListener('click', (e) => {
+        e.stopPropagation();
+    })
+}
+
+popup.addEventListener('click', (e) => {
+    e.stopPropagation();
+})
+
+
+
